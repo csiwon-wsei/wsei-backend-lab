c92e533053e2c5cc8650532a3cae20c606e9a139
Cezary Siwoń <siwon.cezary@gmail.com>
2024-03-22 17:57:09 +0100

    lab10 end

---
+++ StatisticRabbitMQSubscriber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace StatisticRabbitMQSubscriber;
+
+public abstract class Program
+{
+    private static readonly string QueueName = "statistics";
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory { HostName = "localhost" }; 
+        var connection = factory.CreateConnection(); 
+        using var channel = connection.CreateModel();
+        channel.QueueDeclare(QueueName, exclusive: false);
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, eventArgs) =>
+        {
+            var body = eventArgs.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine(message);
+        };
+        channel.BasicConsume(queue: QueueName, autoAck: true, consumer: consumer);
+        Console.ReadKey();
+    }
+}
