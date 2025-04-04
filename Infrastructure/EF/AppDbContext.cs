8bcd0d301367dee7a93792fab74d960e649f0d25
cs <siwon.cezary@gmail.com>
2025-04-04 18:25:00 +0200

    init

--- Infrastructure/EF/AppDbContext.cs
+++ Infrastructure/EF/AppDbContext.cs
@@ -1,16 +1,23 @@
 ﻿using Infrastructure.EF.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+
 namespace Infrastructure.EF;
 
 public class AppDbContext : IdentityDbContext<UserEntity>
 {
-    public DbSet<UserEntity> Users { get; set; }
-
+    public AppDbContext()
+    {
+    }
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
-        optionsBuilder.UseSqlite(@"Data Source=c:\data\app.db");
+        //optionsBuilder.UseSqlite(@"Data Source=c:\data\app.db");
         // optionsBuilder.UseSqlServer(
         //     "DATA SOURCE=CEES\\SQLEXPRESS;DATABASE=appdb;Integrated Security=true;TrustServerCertificate=True");
     }
@@ -21,7 +28,6 @@ public class AppDbContext : IdentityDbContext<UserEntity>
         var adminId = "CDE2D5ED-B13B-4373-BBE4-A1FE8E36CB64";
         var adminBirthDate = new DateTime(2000, 10, 01);
         var adminCreatedAt = new DateTime(2025, 04, 03);
-        // Configure the owned entity
         var adminUser = new UserEntity
         {
             Id = adminId,
@@ -32,7 +38,7 @@ public class AppDbContext : IdentityDbContext<UserEntity>
             NormalizedUserName = "ADMIN@WSEI.EDU.PL",
             ConcurrencyStamp = adminId,
             SecurityStamp = adminId,
-            PasswordHash = "81dc9bdb52d04dc20036dbd8313ed055",
+            PasswordHash = "AQAAAAIAAYagAAAAEGfrCHpubDs17CUOTJKrewMiXqJ/htzvdfA0Ohqj8ZH1bUtar66Cm8LbfjT/JqEUBA=="
         };
         builder.Entity<UserEntity>()
             .HasData(adminUser);
